4bd0f07effec8c2c895208b8ca4bb43a7ca8e629
Javier Alvarado <94930969+javier100ramos@users.noreply.github.com>
2026-07-17 21:30:38 -0600

    Estructura inicial del proyecto Teocalli con Firebase

---
+++ firebase-config.js
@@ -0,0 +1,21 @@
+// Firebase SDK v10 Modular configuration
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+// TODO: Reemplazar con las credenciales de tu proyecto de Firebase
+const firebaseConfig = {
+  apiKey: "TU_API_KEY_AQUI",
+  authDomain: "TU_AUTH_DOMAIN_AQUI",
+  projectId: "TU_PROJECT_ID_AQUI",
+  storageBucket: "TU_STORAGE_BUCKET_AQUI",
+  messagingSenderId: "TU_MESSAGING_SENDER_ID_AQUI",
+  appId: "TU_APP_ID_AQUI"
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+
+// Inicializar servicios
+export const auth = getAuth(app);
+export const db = getFirestore(app);
